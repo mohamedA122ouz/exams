bea60aa4d175e2802738ab01b878603a7138ef7f
nooreldin2735 <nooreldin2735@gmail.com>
2026-01-31 16:05:18 +0200

    added stream to all users

--- socket_encoder/socoket_utils/hub_handler.py
+++ socket_encoder/socoket_utils/hub_handler.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from core.models.Exams_models import messages,classRoom
 from socket_encoder.socoket_utils.group_service import GroupService
 from channels.db import database_sync_to_async
+from core.services.classRoomService import classRoomService
+
 class HubHandler:
     def __init__(self, group_service: GroupService):
         self.group_service = group_service
@@ -21,7 +23,30 @@ class HubHandler:
         user = scope["user"]
         if not user or not user.is_authenticated:
             return False
+        # check if the user is authorized or can acces the class room
+        if not await self.__is_authorized(scope):
+            return False
         return True
+    #-----------------------------------------------
+
+    async def __is_authorized(self,scope:dict)->bool:
+       # create clas room based on current user scope
+       user= scope["user"]
+       class_room_service=classRoomService(user)
+       goup_name= self.group_service.group_name
+
+       class_room=await self.get_class_room_instance(self.group_service.group_name)
+
+       # check if the user is authorized or can acces the class room
+       @database_sync_to_async
+       def check_class_privelges():
+        auth_resource= class_room_service.accessClassRoom(class_room)
+        if not auth_resource["isSuccess"]:
+            return False
+        return True
+
+       return await check_class_privelges()
+    #-----------------------------------------------
     async def save_message(self, message: str|None,sender:UserLazyObject|None)->None:
 
         # save the message into db or redis
@@ -36,7 +61,7 @@ class HubHandler:
         
         await _save()
         
-    async def get_class_room_instance(self,class_room_id:str)->classRoom|None:
+    async def get_class_room_instance(self,class_room_id:str)->classRoom:
         @database_sync_to_async
         def _get():
             try:
